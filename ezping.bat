7fa552f8949825b25567d387828f0a858be153d9
fgclue <schefferdaudtfelipe@gmail.com>
2022-02-22 16:21:07 -0300

    put ezping

---
+++ ezping.bat
@@ -0,0 +1,12 @@
+@echo off
+echo                            oo                   
+echo  ezping                                               
+echo .d8888b. d888888b 88d888b. dP 88d888b. .d8888b. 
+echo 88ooood8    .d8P' 88'  `88 88 88'  `88 88'  `88 
+echo 88.  ...  .Y8P    88.  .88 88 88    88 88.  .88 
+echo `88888P' d888888P 88Y888P' dP dP    dP `8888P88 
+echo                   88                        .88 
+echo                   dP                    d8888P  
+
+set /P website="Choose Website: "
+ping %website%
